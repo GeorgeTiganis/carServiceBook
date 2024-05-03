ec1b474e59352a64748bcb0d2fa7e61fb8a0527f
GeorgeTiganis <giwrgostiganis@gmail.com>
2024-05-03 23:36:09 +0300

    λειρουργει

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react'; 
 
 export default function App() {
+const [ name, setName] = useState('george',);
+const [person, setPerson] = useState({name:'hghg' , age: 22});
+
+const clickHandler = ()=>{
+  setName('chun-li');
+  setPerson({name:'giorgos',age:26})
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is{name}</Text>
+     <Text>ΤΟ ΟΝΟΜΑ ΤΗΣ ΕΙΝΑΙ{person.name}και η ηλικεια τησ ειναι {person.age}</Text>
+     <View style={styles.buttonConatiner}>
+          <Button title='Add new car' onPress={clickHandler}/>
+     </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonConatiner:{
+    
+  }
 });
