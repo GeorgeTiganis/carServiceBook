5c78a08344c3a960b53e47aedcc9c28b171a0cf0
GeorgeTiganis <giwrgostiganis@gmail.com>
2024-05-04 19:06:46 +0300

    Lesson 9 - Todo App - Header - Component

--- App.js
+++ App.js
@@ -1,52 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button , ScrollView, FlatList, TouchableOpacity} from 'react-native';
+
+import { StyleSheet, Text, View, FlatList, } from 'react-native';
 import React,{useState} from 'react'; 
+import Header from './components/header';
 
 export default function App() {
-const [ people, setPeople] = useState([
- {name:'giorgos', id:'1'},
- {name:'mari', id:'2'},
- {name:'stelios', id:'3'},
- {name:'giannhs', id:'4'},
- {name:'periklhe', id:'5'},
- {name:'kaka', id:'6'},
- {name:'paidi', id:'7'},
+
+const [todos,setTodos]= useState([
+  {text:'buy coffe', key:'1'},
+  {text:'create my app', key:'2'},
+  {text:'play on the switch', key:'3'}
 ]);
-
-
-const pressHandler = (id) => {
-  console.log(id);
-  setPeople( (prpevPeople) =>  {
-     return prpevPeople.filter(person =>  person.id != id) 
-  });
-}
 
   return (
     <View style={styles.container}>
-
-      <FlatList
-         
-         keyExtractor={(item)=> item.id}
-         data={ people }
-         renderItem={({item})=>(
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-              <Text style={styles.item}>  {item.name} </Text>
-          </TouchableOpacity>
-          
-         )}
-      />
-
-      {/* <ScrollView>
-      {people.map(item =>(
-        (
-          <View key={item.key}> 
-            <Text style={styles.item}>
-              {item.name}
-            </Text>
-          </View>
-        )
-      ))}
-      </ScrollView> */}
+         <Header/>
+         <View style={styles.content}> 
+            {/**To form */}
+            <View style={styles.lists}>
+                 <FlatList
+                    data={todos}
+                    renderItem={({item}) => (
+                      <Text>{item.text}</Text>
+                    )}
+                  />
+            </View>
+        </View>
+      
      
     </View>
   );
@@ -56,15 +35,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop:40,
-    paddingHorizontal:20,
-    //alignItems: 'center',
-   // justifyContent: 'center',
+   
   },
- item:{
-  marginTop:44,
-  padding:30,
-  backgroundColor:'pink',
-  fontSize:34,
- }
+  content:{
+     padding:40,
+
+  },
+  lists:{
+    marginTop:40,
+  },
+ 
 });
---
+++ components/header.js
@@ -0,0 +1,32 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; 
+
+
+
+export default function Header(){
+    return(
+        <View style={styles.header}>
+            <Text style={styles.title}>My car Book</Text>
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+     header:{
+       height:80,
+       paddingTop:38,
+       backgroundColor:'coral'
+     },
+     title:{
+       textAlign:'center',
+       color:'#fff',
+       fontSize: 20,
+       fontWeight: 'bold'
+        
+    }
+
+
+});
