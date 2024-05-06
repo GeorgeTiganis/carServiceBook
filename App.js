bf0ea4fe4c71cafc831fe97ed53913e0fb83e06f
GeorgeTiganis <giwrgostiganis@gmail.com>
2024-05-06 22:44:25 +0300

    εχω κανει μεχρι και το Drawer navigation

--- App.js
+++ App.js
@@ -1,68 +1,88 @@
-
-import { StyleSheet, Text, View, FlatList, Alert,TouchableWithoutFeedback, Keyboard} from 'react-native';
-import React,{useState} from 'react'; 
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Header from './components/header';
 import TodoItem from './components/todoitem';
 import AddTodo from './components/addTodo';
+import * as Font from 'expo-font';
+import 'react-native-gesture-handler';
+
+const Drawer = createDrawerNavigator();
+
+// Εδώ βάζουμε την γραμματοσειρά που θέλουμε
+const getFonts = () => Font.loadAsync({
+  'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
+});
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffe', key: '1' },
+    { text: 'create my app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
 
-const [todos,setTodos]= useState([
-  {text:'buy coffe', key:'1'},
-  {text:'create my app', key:'2'},
-  {text:'play on the switch', key:'3'}
-]);
-
-const pressHandler = (key) => {
-  setTodos((prevTodos)=> {
-    return prevTodos.filter(todo => todo.key !=key );
-
-
-  });
-}
-
-const submitHandler = (text) => {
-
-  if (text.length > 3 ){
+  const pressHandler = (key) => {
     setTodos((prevTodos) => {
-      return [
-        {text:text, key:Math.random().toString()},
-        ...prevTodos
-      ]
-  
+      return prevTodos.filter(todo => todo.key != key);
     });
-  }else {
-      Alert.alert('oops!','Ξαναπροσπαθησε  , εχεις εισαγει λαθος χαρακτηρες',[
-        {text:'understood', onPess:() => console.log('alert closed')}
-      ]);
   }
- 
-}
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('Oops!', 'Ξαναπροσπαθήστε, έχετε εισάγει λανθασμένους χαρακτήρες', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  }
 
   return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home">
+          {() => (
+            <TouchableWithoutFeedback onPress={() => {
+              Keyboard.dismiss();
+              console.log('dismissed keyboard')
+            }}>
+              <View style={styles.container}>
+                <Header />
+                <View style={styles.content}>
+                  <AddTodo submitHandler={submitHandler} />
+                  <View style={styles.lists}>
+                    <FlatList
+                      data={todos}
+                      renderItem={({ item }) => (
+                        <TodoItem item={item} pressHandler={pressHandler} />
+                      )}
+                    />
+                  </View>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          )}
+        </Drawer.Screen>
+        <Drawer.Screen name="About" component={AboutScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
 
-    <TouchableWithoutFeedback onPress={() =>{
-      Keyboard.dismiss();
-      console.log('dismissed keyboard')
-    }}>
+function AboutScreen() {
+  return (
     <View style={styles.container}>
-         <Header/>
-         <View style={styles.content}> 
-            <AddTodo submitHandler={submitHandler}/>
-            <View style={styles.lists}>
-                 <FlatList
-                    data={todos}
-                    renderItem={({item}) => (
-                     <TodoItem item={item} pressHandler={pressHandler} />
-
-                    )}
-                  />
-            </View>
-          </View>
-      
-     
+      <Header />
+      <View style={styles.content}>
+        <Text>About Screen</Text>
+      </View>
     </View>
-    </TouchableWithoutFeedback>
   );
 }
 
@@ -70,14 +90,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-   
   },
-  content:{
-     padding:40,
-
+  content: {
+    padding: 40,
   },
-  lists:{
-    marginTop:40,
+  lists: {
+    marginTop: 40,
   },
- 
 });
